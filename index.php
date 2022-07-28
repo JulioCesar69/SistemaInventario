f3307d2cb2aebce01c3aa43011baf84a9771fa92
JulioCesar69 <juliocrack912@gmail.com>
2022-07-28 15:10:38 -0500

    creando vista home, login y 404

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
+<?php require "./inc/sesion_start.php";?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <?php include "./inc/head.php";?>
 </head>
 <body>
-<?php include "./inc/navbar.php";?>
-<?php include "./inc/script.php";?>
+    <?php 
+        if(!isset($_GET['vista']) || $_GET['vista']==""){
+            $_GET['vista']=="login";
+        }
+        if (is_file("./vistas/".$_GET['vista'].".php") && $_GET['vistas']!="login" && $_GET['vistas']!="404"){
+            include "./inc/navbar.php";
+            include "./vistas/".$_GET['vista'].".php";
+            include "./inc/script.php";    
+        } else {
+            if($_GET['vista']=="login"){
+                include "./vistas/login.php";
+            }else{
+                include "./vistas/404.php";
+            }
+        }
+        
+    ?>
 </body>
 </html>
