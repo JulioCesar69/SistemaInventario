f0ec085ff32cb075ebc3c23689c2d15db60006fe
JulioCesar69 <juliocrack912@gmail.com>
2022-07-28 15:39:42 -0500

    Validar expresiones de formularios

--- php/main.php
+++ php/main.php
@@ -4,4 +4,16 @@
         $pdo=new PDO('mysql:host=localhost;dbname=inventario','root',''); 
         return $pdo;
     }  
+    //funcion para verificar datos
+    function verificar_datos($filtro,$cadena){
+        if (preg_match("/^".$filtro."$/",$cadena)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    $nombre="carlos7";
+    if(verificar_datos("[a-zA-A]{6,10}",$nombre)){
+        echo "Los datos no coinciden";
+    }
 ?>
