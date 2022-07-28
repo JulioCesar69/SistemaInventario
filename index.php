4f1feff3b4584db14aa5e7b3cfe8d18179f04f93
JulioCesar69 <juliocrack912@gmail.com>
2022-07-28 14:00:39 -0500

    Incluyendo estilos y framework Bulma

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de inventario</title>
+    <?php include "./inc/head.php";?>
 </head>
 <body>
-    
+<?php include "./inc/navbar.php";?>
+<?php include "./inc/script.php";?>
 </body>
 </html>
